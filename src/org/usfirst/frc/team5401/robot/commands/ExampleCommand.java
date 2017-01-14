3002db712b65987283f77cfe243a57dbc422701a
Kevin McKay <kmckay@bensalemsd.org>
2017-01-13 20:25:26 -0500

    Basic Setup
    Doesn't deploy

--- src/org/usfirst/frc/team5401/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team5401/robot/commands/ExampleCommand.java
@@ -8,25 +8,40 @@ import org.usfirst.frc.team5401.robot.Robot;
  *
  */
 public class ExampleCommand extends Command {
+	
+	private int i;
+	private boolean done;
+	
 	public ExampleCommand() {
 		// Use requires() here to declare subsystem dependencies
 		requires(Robot.exampleSubsystem);
+		i = 0;
+		done = false;
 	}
 
 	// Called just before this Command runs the first time
 	@Override
+
 	protected void initialize() {
+		i = 0;
+		done = false;
+
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
+		Robot.exampleSubsystem.DisplayTables();
+		i = i+1;;
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return false;
+		if (i > 1000) {
+			done = true;
+		}
+		return done;
 	}
 
 	// Called once after isFinished returns true
--- src/org/usfirst/frc/team5401/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/ExampleSubsystem.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5401.robot.subsystems;
 
+import org.usfirst.frc.team5401.robot.commands.ExampleCommand;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.*;
 
 /**
  *
@@ -8,9 +11,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ExampleSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-
+	  static NetworkTable RioTable;
+	  double NetworkX = 0;
+	  double NetworkY = 0;
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(new ExampleCommand());
+	}
+	
+	public void DisplayTables() {
+	   NetworkX =  RioTable.getNumber("Y",-99);
+	   NetworkY = RioTable.getNumber("X",-99);
+		System.out.println(NetworkX);
+		System.out.println(NetworkY);
 	}
 }
